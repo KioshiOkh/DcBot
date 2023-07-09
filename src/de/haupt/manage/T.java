efc1359e6fef32934b3dfe7bf77dcc5ff6a78dab
Paul | Kioshi <116959356+KioshiOkh@users.noreply.github.com>
2023-07-09 21:33:47 +0200

    Update T.java

--- src/de/haupt/manage/T.java
+++ src/de/haupt/manage/T.java
@@ -3,7 +3,7 @@ package de.haupt.manage;
 public class T
 {
 	
-public String TOKEN = "ODQzMTM5MDUzNTI4Njc4NDQy.YJ_gbw.RTztn-UwHs-7-x_mt2Rp5D-wcdk";
+public String TOKEN = "NOMORETOKEN";
 public String LILITH = "843139053528678442";
 
 }
